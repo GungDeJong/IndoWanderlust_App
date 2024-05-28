8370cf0a70c5d501026af89ec537537a48b9424f
Agung <vivopradnyana@gmail.com>
2024-05-28 21:34:40 +0700

    Second Commit

---
+++ lib/utils/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+Color backgroundwhitecolor = Color(0xFFFFFFFF);
+Color backgroundblueskcolory = Color(0xFF0FA3E2);
+Color fontblackcolor = Color(0xFF000000);
+Color fontwhitecolor = Color(0xFFFFFFFF);
+Color fontbluecolor = Color(0xFF0061D2);
+Color grayColor = Color(0xFF797979);
+Color dividerColor = Color(0xFF9D9D9D);
+Color bordeColor = Color(0xFFE2E2E2);
+Color grayButtoon = Color(0xFFFAFAFA);
+Color formColor = Color(0xFFF7F7F7);
+Color hintColor = Color(0xFFB3B3B3);
