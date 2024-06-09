1edfcedb772477db7c482cef38d8c8ca9bdd86e3
Akmal <farhanakmal@student.telkomuniversity.ac.id>
2024-06-10 06:08:49 +0700

    Update Profile page to Connect to Firebase Auth and Firestore Database

--- lib/views/profile_view.dart
+++ lib/views/profile_view.dart
@@ -1,102 +1,115 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class ProfileView extends StatelessWidget {
+class ProfileView extends StatefulWidget {
   const ProfileView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  _ProfileViewState createState() => _ProfileViewState();
+}
+
+class _ProfileViewState extends State<ProfileView> {
+  String fullName = 'Loading...'; // Default value until fetched from Firestore
+  String userName = '';
+  String email = '';
+
+  @override
+  void initState() {
+    super.initState();
     // Get the current user from Firebase Authentication
     User? user = FirebaseAuth.instance.currentUser;
 
     // Check if the user is logged in
     if (user != null) {
-      // If the user is logged in, extract user information
-      String displayName = user.displayName ?? '';
-      String email = user.email ?? '';
-
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('Profile'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 50,
-                child: Icon(Icons.person, size: 50),
-              ),
-              SizedBox(height: 20),
-              Text(
-                displayName, // Use the display name of the user
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 10),
-              Text(
-                email, // Use the email of the user
-                style: TextStyle(fontSize: 16, color: Colors.grey),
-              ),
-              ElevatedButton(
-                onPressed: () async {
-                  // Sign out the user
-                  await FirebaseAuth.instance.signOut();
-                  // Navigate back to the home screen or any other screen
-                  Navigator.pop(context);
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.red,
-                ),
-                child: Text(
-                  'Logout',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ],
-
-          ),
-        ),
-      );
-    }else {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('Profile'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Login to open your profile',
-                style: TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 20.0),
-              ElevatedButton(
-                onPressed: () {
-                  // Navigate to the login screen
-                  Navigator.pushNamed(context, '/login');
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                ),
-                child: Text(
-                  'Login',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      );
+      // If the user is logged in, fetch user data from Firestore
+      fetchUserData(user.uid); // Pass user's UID
     }
+  }
+
+  Future<void> fetchUserData(String userID) async {
+    try {
+      // Query Firestore to get the user data based on userID
+      DocumentSnapshot userSnapshot = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(userID)
+          .get();
+
+      if (userSnapshot.exists) {
+        setState(() {
+          fullName = userSnapshot['fullName'];
+          userName = userSnapshot['username'];
+          email = userSnapshot['email'];
+        });
+      } else {
+        print('No user found for userID: $userID');
+      }
+    } catch (e) {
+      print('Error fetching user data: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50,
+              child: Icon(Icons.person, size: 50),
+            ),
+            SizedBox(height: 20),
+            Text(
+              fullName,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '$userName',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '$email',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () async {
+                // Sign out the user
+                await FirebaseAuth.instance.signOut();
+                // Navigate back to the home screen or any other screen
+                Navigator.pop(context);
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+              ),
+              child: Text(
+                'Logout',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
