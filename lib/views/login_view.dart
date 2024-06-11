8e8c7f43f121085f44223de528afcae7eed63e61
gibranariqn <gibranariq15@gmail.com>
2024-06-11 10:35:28 +0700

    Fixing login page

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -19,10 +19,10 @@ class _LoginViewState extends State<LoginView> {
 
   void _signInWithEmailPassword() async {
     try {
-    await _auth.signInWithEmailAndPassword(
-      email: emailController.text,
-      password: passwordController.text,
-    );
+      await _auth.signInWithEmailAndPassword(
+        email: emailController.text,
+        password: passwordController.text,
+      );
 
       // Show a success message
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
@@ -48,128 +48,129 @@ class _LoginViewState extends State<LoginView> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(16, 40, 16, 0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Image.asset(
-                    "assets/images/image4.png",
-                    width: 44,
-                    height: 44,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20.0, bottom: 32),
-                    child: Text(
-                      "Welcome To IndoWanderlust",
-                      style: TextStyle(
-                          fontFamily: "PoppinsSemiBold", fontSize: 20),
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(16, 40, 16, 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Image.asset(
+                      "assets/images/image4.png",
+                      width: 44,
+                      height: 44,
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 8),
-                    child: Text(
-                      "Enter your email and password to continue",
-                      style: TextStyle(
-                          fontFamily: "PopppinsRegular",
-                          fontSize: 14,
-                          color: grayColor),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20.0, bottom: 32),
+                      child: Text(
+                        "Welcome To IndoWanderlust",
+                        style: TextStyle(
+                            fontFamily: "PoppinsSemiBold", fontSize: 20),
+                      ),
                     ),
-                  ),
-                  Text("Email",
-                      style: TextStyle(
-                          fontFamily: "PoppinsRegular", fontSize: 14)),
-                  TextFormField(
-                    controller: emailController,
-                    style:
-                        TextStyle(fontFamily: "PopppinsRegular", fontSize: 14),
-                    decoration: InputDecoration(
-                        contentPadding: EdgeInsets.symmetric(
-                            vertical: 13.5, horizontal: 16),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(4),
-                            borderSide:
-                                BorderSide(width: 0, style: BorderStyle.none)),
-                        fillColor: formColor,
-                        filled: true,
-                        hintText: "name@gmail.com",
-                        focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                                color: backgroundblueskcolory, width: 2.0)),
-                        hintStyle: TextStyle(
-                          color: hintColor,
-                          fontFamily: "PopppinsRegular",
-                          fontSize: 14,
-                        )),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20),
-                    child: Text("Password",
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8),
+                      child: Text(
+                        "Enter your email and password to continue",
+                        style: TextStyle(
+                            fontFamily: "PopppinsRegular",
+                            fontSize: 14,
+                            color: grayColor),
+                      ),
+                    ),
+                    Text("Email",
                         style: TextStyle(
                             fontFamily: "PoppinsRegular", fontSize: 14)),
-                  ),
-                  TextFormField(
-                    controller: passwordController,
-                    obscureText: isHide,
-                    style:
-                        TextStyle(fontFamily: "PopppinsRegular", fontSize: 14),
-                    decoration: InputDecoration(
-                        suffixIcon: IconButton(
-                          onPressed: () {
-                            isHide = !isHide;
-                            setState(() {});
-                          },
-                          icon: Icon(
-                            isHide
-                                ? Icons.visibility_outlined
-                                : Icons.visibility_off_outlined,
-                            size: 18,
-                            color: hintColor,
-                          ),
-                        ),
-                        contentPadding: EdgeInsets.symmetric(
-                            vertical: 13.5, horizontal: 16),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(4),
-                            borderSide:
-                                BorderSide(width: 0, style: BorderStyle.none)),
-                        fillColor: formColor,
-                        filled: true,
-                        hintText: "Input Password",
-                        focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                                color: backgroundblueskcolory, width: 2.0)),
-                        hintStyle: TextStyle(
+                    TextFormField(
+                      controller: emailController,
+                      style: TextStyle(
+                          fontFamily: "PopppinsRegular", fontSize: 14),
+                      decoration: InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(
+                              vertical: 13.5, horizontal: 16),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(4),
+                              borderSide: BorderSide(
+                                  width: 0, style: BorderStyle.none)),
+                          fillColor: formColor,
+                          filled: true,
+                          hintText: "name@gmail.com",
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: backgroundblueskcolory, width: 2.0)),
+                          hintStyle: TextStyle(
                             color: hintColor,
                             fontFamily: "PopppinsRegular",
-                            fontSize: 14)),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 32),
-                    child: GestureDetector(
-                      onTap: _signInWithEmailPassword,
-                      child: Container(
-                        width: double.infinity,
-                        padding: EdgeInsets.symmetric(vertical: 13.5),
-                        decoration: BoxDecoration(
-                            border: Border.all(color: bordeColor),
-                            color: backgroundblueskcolory,
-                            borderRadius: BorderRadius.circular(4)),
-                        child: Text(
-                          "Sign In",
-                          textAlign: TextAlign.center,
+                            fontSize: 14,
+                          )),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: Text("Password",
                           style: TextStyle(
-                              fontFamily: "PoppinsSemiBold",
-                              fontSize: 14,
-                              color: fontwhitecolor),
+                              fontFamily: "PoppinsRegular", fontSize: 14)),
+                    ),
+                    TextFormField(
+                      controller: passwordController,
+                      obscureText: isHide,
+                      style: TextStyle(
+                          fontFamily: "PopppinsRegular", fontSize: 14),
+                      decoration: InputDecoration(
+                          suffixIcon: IconButton(
+                            onPressed: () {
+                              isHide = !isHide;
+                              setState(() {});
+                            },
+                            icon: Icon(
+                              isHide
+                                  ? Icons.visibility_outlined
+                                  : Icons.visibility_off_outlined,
+                              size: 18,
+                              color: hintColor,
+                            ),
+                          ),
+                          contentPadding: EdgeInsets.symmetric(
+                              vertical: 13.5, horizontal: 16),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(4),
+                              borderSide: BorderSide(
+                                  width: 0, style: BorderStyle.none)),
+                          fillColor: formColor,
+                          filled: true,
+                          hintText: "Input Password",
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: backgroundblueskcolory, width: 2.0)),
+                          hintStyle: TextStyle(
+                              color: hintColor,
+                              fontFamily: "PopppinsRegular",
+                              fontSize: 14)),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 32),
+                      child: GestureDetector(
+                        onTap: _signInWithEmailPassword,
+                        child: Container(
+                          width: double.infinity,
+                          padding: EdgeInsets.symmetric(vertical: 13.5),
+                          decoration: BoxDecoration(
+                              border: Border.all(color: bordeColor),
+                              color: backgroundblueskcolory,
+                              borderRadius: BorderRadius.circular(4)),
+                          child: Text(
+                            "Sign In",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontFamily: "PoppinsSemiBold",
+                                fontSize: 14,
+                                color: fontwhitecolor),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
@@ -180,18 +181,18 @@ class _LoginViewState extends State<LoginView> {
               children: [
                 Text(
                   "Login as a ",
-                  style:
-                      TextStyle(fontFamily: "PoppinsRegular", color: grayColor),
+                  style: TextStyle(
+                      fontFamily: "PoppinsRegular", color: grayColor),
                 ),
                 GestureDetector(
-                  onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => HomeView())),
+                  onTap: () => Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => HomeView())),
                   child: Text(
                     "Guest",
                     style: TextStyle(
                         fontFamily: "PoppinsSemiBold", color: fontbluecolor),
                   ),
-                )
-
+                ),
               ],
             ),
           ),
@@ -206,17 +207,17 @@ class _LoginViewState extends State<LoginView> {
                   TextStyle(fontFamily: "PoppinsRegular", color: grayColor),
                 ),
                 GestureDetector(
-                  onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => RegisterView())),
+                  onTap: () => Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => RegisterView())),
                   child: Text(
                     "Register",
                     style: TextStyle(
                         fontFamily: "PoppinsSemiBold", color: fontbluecolor),
                   ),
-                )
-
+                ),
               ],
             ),
-          )
+          ),
         ],
       ),
     );
